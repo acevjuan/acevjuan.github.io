822b90565c016e62b006de492b140a466022a634
juanmarcos <acevedojuan17@gmail.com>
2022-09-04 14:43:39 -0500

    Creación de función para modificar recetas ya creadas.

--- js/funciones.js
+++ js/funciones.js
@@ -18,6 +18,37 @@ function crearReceta() {
     console.table(recetasGuardadas);
 }
 
+function modificarReceta() {
+    let recetaModificar = parseInt(prompt('Indicar el ID de la receta a modificar'));
+    if (recetaModificar < recetasGuardadas.length) {
+        let eo = parseFloat(prompt('¿Cuánto es el nuevo extracto original deseado?'));
+        let vol = parseFloat(prompt('¿Cuántos litros de cerveza quieres elaborar?'));
+        let extMal = parseFloat(prompt('¿Cuál es el nuevo extracto de la malta a utilizar?'));
+        let hMal = parseFloat(prompt('¿Cuál es la nueva humedad de la malta?'));
+
+        //Calculo de gravedad específica.
+        let sg = ((eo * 4)/1000) + 1;
+
+        //Cálculo de cantidad de malta necesaria para la receta.
+        let cantMal = (vol * sg * densidadAgua * eo) / (extMal * (1 - hMal) * eficiencia);
+
+        let confirmacionModificar = confirm('¿Modificar receta?');
+        if(confirmacionModificar === true) {
+            recetasGuardadas[recetaModificar].extractoOriginal = eo;
+            recetasGuardadas[recetaModificar].volumen = vol;
+            recetasGuardadas[recetaModificar].extractoMalta = extMal;
+            recetasGuardadas[recetaModificar].humedadMalta = hMal;
+            recetasGuardadas[recetaModificar].cantidadMalta = cantMal;
+            alert('Receta modificada con éxito')
+        } else {
+            alert('Proceso finalizado. No existen cambios');
+        }
+    } else {
+        alert('Proceso finalizado. No existen cambios');
+    }
+    console.table(recetasGuardadas);
+}
+
 function eliminarReceta() {
     let recetaEliminar = parseInt(prompt('Indicar el ID de la receta a eliminar'));
     if (recetaEliminar < recetasGuardadas.length) {
@@ -39,7 +70,3 @@ function imprimirRecetas() {
         console.log(`Para elaborar ${recetasGuardadas[i].volumen} litros (L) de la cerveza ${recetasGuardadas[i].nombre} con extracto original de ${recetasGuardadas[i].extractoOriginal * 100} ºP, se requiere ${recetasGuardadas[i].cantidadMalta} kg de cebada malteada`);
     }
 }
-
-//Crear función que modifique recetas creadas. Usar como criterio de busqueda un condicional que pregunta si es por id o por nombre de la receta.
-
-//Crear función que describa de forma genérica cómo hacer la receta con una iteración a todos los elementos del objeto.
