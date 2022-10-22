b98cf60fd3c5e9aaf55d43ec16672b4fdb20a59e
juanmarcos <acevedojuan17@gmail.com>
2022-10-22 16:40:17 -0500

    Incluyendo valores iniciales de prueba en creación de recetas para facilitar revisión

--- js/script.js
+++ js/script.js
@@ -35,3 +35,10 @@ fetch('bbdd/guia-extracto-original.json')
         })
     })
     .catch((error) => console.error('ERROR', error))
+
+//Inclusión de valores prederteminados en formulario de creación de recetas, para facilitar corrección por parte del profesor.
+document.querySelector('#nombre').value = 'Cerveza de prueba';
+document.querySelector('#extracto').value = 11;
+document.querySelector('#volumen').value = 50;
+document.querySelector('#ext-mal').value = 82;
+document.querySelector('#humedad').value = 4;
